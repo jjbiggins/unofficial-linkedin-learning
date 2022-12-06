65145ff6503a417af1048ca8805c90d653b0c777
jjbiggins <jjbiggins+github@biggins.tech>
2022-12-05 21:47:11 -0600

    Initial Commit

---
+++ unofficial-linkedin-learning/unofficial_linkedin_learningApp.swift
@@ -0,0 +1,17 @@
+//
+//  unofficial_linkedin_learningApp.swift
+//  unofficial-linkedin-learning
+//
+//  Created by Joe Biggins on 12/5/22.
+//
+
+import SwiftUI
+
+@main
+struct unofficial_linkedin_learningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
